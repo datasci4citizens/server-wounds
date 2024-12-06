4084164ff82e5948a54060f2b52a6ce14998f6dc
André Santanchè <santanche@gmail.com>
2024-12-06 16:03:22 -0300

    fix (session): session verification

--- src/auth/auth_service.py
+++ src/auth/auth_service.py
@@ -3,6 +3,6 @@ from fastapi import HTTPException, Request
 class AuthService:
     @staticmethod
     def get_current_user(request: Request):
-        if not hasattr(request, 'session') or not request.session:
+        if not request.session or request.session.get("email") is None:
             raise HTTPException(status_code=401, detail="User not authenticated")
         return request.session.get("email")
