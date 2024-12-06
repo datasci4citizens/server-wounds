deed5d01ae27bdfcc7bc580873dc16ee9b18e6ce
André Santanchè <santanche@gmail.com>
2024-12-06 15:47:03 -0300

    fix (session): credentials removed

--- src/auth/auth_service.py
+++ src/auth/auth_service.py
@@ -3,7 +3,6 @@ from fastapi import HTTPException, Request
 class AuthService:
     @staticmethod
     def get_current_user(request: Request):
-        credentials = request.session.get("credentials")
-        if not credentials or not credentials.get("token"):
+        if not hasattr(request, 'session') or not request.session:
             raise HTTPException(status_code=401, detail="User not authenticated")
         return request.session.get("email")
