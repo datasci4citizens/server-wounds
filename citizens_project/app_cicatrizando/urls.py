2797fbe427af0672109ffadda201778429dfb8de
GoliasVictor <dev.jvsb@gmail.com>
2025-05-02 05:51:40 -0300

    feat: adiciona geração de schema openapi e visualização da api usando swagger e scalar

--- citizens_project/app_cicatrizando/urls.py
+++ citizens_project/app_cicatrizando/urls.py
@@ -2,6 +2,14 @@ from django.urls import include, path
 
 from . import views
 
+# from django_scalar.views import scalar_viewer
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from app_cicatrizando.scalar import scalar_viewer
+
+
 urlpatterns = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger", SpectacularSwaggerView.as_view(), name="schema-swagger-ui"),
+    path("api/schema/scalar", scalar_viewer, name="schema-scalar-ui"), 
     path('about/app_cicatrizando/', views.index, name="index"),
 ]
