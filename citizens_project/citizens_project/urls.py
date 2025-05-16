c09e3857874143efd310f6c85a8f7a57afc73b8e
GoliasVictor <dev.jvsb@gmail.com>
2025-05-16 01:58:01 -0300

    fix: conserta rotas

--- citizens_project/citizens_project/urls.py
+++ citizens_project/citizens_project/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import api
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(api.router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('app_cicatrizando.urls')),
     path('admin/', admin.site.urls),
