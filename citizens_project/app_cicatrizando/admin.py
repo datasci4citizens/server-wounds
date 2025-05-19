06a4454aa5d48eacd2028adfeb4b7d79a2695af2
g169366 <g169366@dac.unicamp.br>
2025-05-18 23:33:05 -0300

    models_omop: mapeando campos

--- citizens_project/app_cicatrizando/admin.py
+++ citizens_project/app_cicatrizando/admin.py
@@ -1,10 +1,27 @@
 # Register your models here.
 from django.contrib import admin
+from .models import Specialists, Comorbidities, Patients, Images, Wound, TrackingRecords
 
-from .models import Specialists, Patients, Wound, Comorbidities, Images
-
+# Register original models
 admin.site.register(Specialists)
-admin.site.register(Patients)
 admin.site.register(Comorbidities)
+admin.site.register(Patients)
 admin.site.register(Images)
 admin.site.register(Wound)
+admin.site.register(TrackingRecords)
+
+# Register OMOP models - import inside a try/except block to avoid issues during app loading
+try:
+    from .models_omop import Person, Provider, ConditionConcept, ConditionOccurrence, \
+        NoteAttachment, Observation, Measurement
+        
+    admin.site.register(Person)
+    admin.site.register(Provider)
+    admin.site.register(ConditionConcept)
+    admin.site.register(ConditionOccurrence)
+    admin.site.register(NoteAttachment)
+    admin.site.register(Observation)
+    admin.site.register(Measurement)
+except:
+    # If models can't be imported during the first app load, they'll be registered later
+    pass
