502ca82aae3519915be1f62a66c2ef41060f9164
Ricardo Santos <sdric@amazon.com>
2025-06-05 14:30:14 -0300

    add web/android control logic on google api

--- citizens_project/app_cicatrizando/google.py
+++ citizens_project/app_cicatrizando/google.py
@@ -3,6 +3,8 @@ from typing import Any, Dict
 import requests
 from django.conf import settings
 from rest_framework.exceptions import APIException
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
 
 GOOGLE_ID_TOKEN_INFO_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo"
 GOOGLE_ACCESS_TOKEN_OBTAIN_URL = "https://accounts.google.com/o/oauth2/token"
@@ -43,13 +45,32 @@ def google_get_user_info(access_token: str) -> Dict[str, Any]:
 
     return response.json()
 
-
-def google_get_user_data(validated_data):
+def google_get_user_data_web(code):
     # https://github.com/MomenSherif/react-oauth/issues/252
     redirect_uri = "postmessage"
-
-    code = validated_data.get("code")
-
     access_token = google_get_access_token(code=code, redirect_uri=redirect_uri)
-
     return google_get_user_info(access_token=access_token)
+
+
+def google_get_user_data_mobile(token):
+    try:
+        idinfo = id_token.verify_oauth2_token(
+            token,
+            google_requests.Request(),
+            audience=[settings.GOOGLE_OAUTH2_CLIENT_ID],
+        )
+
+        return {
+            "email": idinfo["email"],
+            "given_name": idinfo.get("given_name", ""),
+            "family_name": idinfo.get("family_name", ""),
+            "sub": idinfo["sub"],
+        }
+    except ValueError:
+        raise Exception("ID token inválido")
+
+def google_get_user_data(validated_data):
+    if validated_data.get("code"):
+        return google_get_user_data_web(code=validated_data["code"])
+    else:
+        return google_get_user_data_mobile(token=validated_data["token"])
