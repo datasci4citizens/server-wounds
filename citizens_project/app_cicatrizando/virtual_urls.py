7cd032f2ea8a836d667dd4a1e8e729dace57c260
GoliasVictor <dev.jvsb@gmail.com>
2025-06-09 15:26:32 -0300

    feat: implementa upload de imagem
    hotfix:
    - Consertei linha row_procedure em VirtualTrackingRecords que possuia chaves que não deveriam ser chave

--- citizens_project/app_cicatrizando/virtual_urls.py
+++ citizens_project/app_cicatrizando/virtual_urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from .virtual_views import(VirtualPatientViewSet, VirtualSpecialistViewSet, VirtualWoundViewSet, 
-                         VirtualTrackingRecordsViewSet, VirtualComorbidityViewSet)
+                         VirtualTrackingRecordsViewSet, VirtualComorbidityViewSet, TrackingRecordsImageViewSet, WoundImageViewSet)
 
 router = DefaultRouter()
 router.register(r'patients', VirtualPatientViewSet)
 router.register(r'specialists', VirtualSpecialistViewSet)
 router.register(r'wounds', VirtualWoundViewSet)
+router.register(r'wounds/image', WoundImageViewSet, basename='wounds-image')
 router.register(r'tracking-records', VirtualTrackingRecordsViewSet)
+router.register(r'tracking-records/image', TrackingRecordsImageViewSet, basename='tracking-records-image')
 router.register(r'comorbidities', VirtualComorbidityViewSet)
