13256b32562ac865bd2dc28873970da9e3cd13f8
Ricardo Dias <85866593+sdiasricardo@users.noreply.github.com>
2025-07-03 09:05:51 -0300

    Update settings.py

--- citizens_project/citizens_project/settings.py
+++ citizens_project/citizens_project/settings.py
@@ -184,7 +184,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Seu frontend
     "http://127.0.0.1:5173",
     "https://server.wounds.staging.paas.ic.unicamp.br",
-    "https://server.wounds.paas.ic.unicamp.br"
+    "https://server.wounds.paas.ic.unicamp.br",
     "https://localhost",
 ]
 
